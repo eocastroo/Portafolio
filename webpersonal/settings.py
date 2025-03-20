3add0fa96f8ac92fcc6b7e845c0e53bd31600643
Edwin Orlando Castro. <edwinorlandocastro@hotmail.com>
2025-03-20 18:47:55 -0500

    Se realiza la configuracin del setting y del gitignore

--- webpersonal/settings.py
+++ webpersonal/settings.py
@@ -10,24 +10,30 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+
 import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+import os
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-lhe&yy5v!mcz1v@g3!lq5a(y)uw%ugoj20d7mb_u1d+s_fpv*7'
+SECRET_KEY = os.environ.get('SECRET_KEY', default='sdfs66fdeddfeds6f6')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'RENDER' not in  os.environ
 
 ALLOWED_HOSTS = []
 
+RENDER_EXTERNA_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNA_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNA_HOSTNAME)
 
 # Application definition
 
@@ -120,14 +126,17 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+if not DEBUG:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-#Archivos Media
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR,'media')
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+
