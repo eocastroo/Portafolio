5b6ca9c92ae22624b073177b4c93e4c7517dae38
Edwin Orlando Castro. <edwinorlandocastro@hotmail.com>
2025-03-21 21:53:01 -0500

    Se procede realizar una nueva configuracion

--- webpersonal/urls.py
+++ webpersonal/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 from django.contrib import admin # type: ignore
 from django.urls import path, include
 from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
     #App core
     path('', include('core.urls')),
@@ -29,5 +30,4 @@ urlpatterns = [
 # Configuracion para la parte Media para el desarrollo
 
 if settings.DEBUG:
-    from django.conf.urls.static import static
     urlpatterns += static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
