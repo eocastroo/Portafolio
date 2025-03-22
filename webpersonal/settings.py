be1f393a58fed949b538bac4c2d07681557d3a1c
Edwin Orlando Castro. <edwinorlandocastro@hotmail.com>
2025-03-21 22:30:31 -0500

    Se proce a realizar configuracion de variabgles

--- webpersonal/settings.py
+++ webpersonal/settings.py
@@ -125,9 +125,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = '/media/'
+MEDIA_URL  = '/media/'
 if not DEBUG:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'media')
+    MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
